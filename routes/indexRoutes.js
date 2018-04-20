1b3cbd486850572ae0034d2e014917a20b3e20aa
NickDErrico <nickderrico@hotmail.com>
2018-04-19 18:54:09 -0700

    refactored index routes

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,9 +1,50 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
+const jwt = require("jsonwebtoken");
+const jwtSecret = "nicknick";
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' })
 });
 
+router.post("/login", function(req, res) {
+  knex('doctors')
+  .where({'email': req.body.email, 'password': req.body.password})
+  .then((doctor) => {
+    doctor = doctor[0];
+    if (!doctor) {
+      res.sendStatus(400);
+    }
+    if (doctor.password === req.body.password) {
+      knex('patients')
+        .where('doctor_id', doctor.id)
+        .then((patients) => {
+          res.json({doctor: doctor, patients: patients, token: jwt.sign({doctor}, jwtSecret)});
+        })
+    } else {
+      res.sendStatus(400);
+    }
+  })
+})
+
+router.post("/register", function(req, res) {
+    knex('doctors').insert({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      password: req.body.password
+    })
+    .then(() => {
+      res.json({doctor: doctor, token: jwt.sign({doctor}, jwtSecret)});
+    })
+})
+
+router.get("/profile", function(req, res) {
+  knex('testResults')
+  .where('patient_id', data.data.patients.id)
+  .then((testResult) => {
+    res.json(testResult)
+  })
+})
 module.exports = router;
--- routes/testRangesRoutes.js
+++ routes/testRangesRoutes.js
@@ -16,34 +16,6 @@ router.get('/single/:id', function(req, res) {
     .then(testRanges => res.json(testRanges[0]))
 });
 
-// router.get('/:id', function(req, res) {
-//   knex('parent_child_component')
-//     .select('components.*')
-//     .innerJoin('components', 'parent_child_component.child_id', 'components.id')
-//     .where('parent_id', req.params.id)
-//     .then(childComponents => {
-//       let component = {
-//           parentId: req.params.id,
-//           childComponents
-//       }
-//       console.log('childComponents', childComponents)
-//       res.json(component)
-//     })
-// });
-//
-// router.post('/', function(req, res) {
-//   knex('components')
-//     .insert(req.body, '*')
-//     .then(newComponent => {
-//       knex('parent_child_component')
-//         .insert({
-//           parent_id: req.body.parent_id,
-//           child_id: newComponent.id
-//         })
-//         .then(() => res.send(newComponent))
-//     })
-// });
-
 router.patch('/:id', function(req, res) {
   knex('testRanges')
     .update(req.body)
